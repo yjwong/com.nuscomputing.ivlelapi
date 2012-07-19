b3bea612dd07267665eeb40277907581b58612b5
Yong Jie Wong <yjwong92@gmail.com>
2012-07-20 03:33:20 +0800

    getPhotoURL() now gets the actual photo URL.
    The IVLE LAPI provides a photo URL that is actually a HTML page that
    contains a single JPEG image. Added Jsoup parser so that this image
    can be extracted from the <img> tags.

--- src/com/nuscomputing/ivlelapi/User.java
+++ src/com/nuscomputing/ivlelapi/User.java
@@ -1,11 +1,20 @@
 package com.nuscomputing.ivlelapi;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Represents a user.
@@ -57,11 +66,17 @@ public class User extends IVLEObject {
 
 	/**
 	 * Method: getDisplayPhotoURL
+	 * <p>
 	 * Obtains the display photo of this user.
+	 * <p>
+	 * WARNING: due to IVLE LAPI's failure to provide an API that gives you
+	 * the image data directly, this method uses blocking calls and cannot
+	 * be used in critical event loops (like UI).
 	 * 
 	 * @param courseID
 	 */
-	public URL getDisplayPhotoURL(String courseID) throws Exception {
+	public URL getDisplayPhotoURL(String courseID) throws
+			MalformedURLException, IOException {
 		// Check for sanity.
 		if (this.ivle.apiKey == null || this.ivle.authToken == null) {
 			throw new IllegalStateException();
@@ -72,13 +87,34 @@ public class User extends IVLEObject {
 		}
 		
 		// Craft the URL.
-		String dpurl_str = "https://ivle.nus.edu.sg/api/DisplayPhoto.ashx?";
-		dpurl_str = dpurl_str.concat("APIKey=").concat(this.ivle.apiKey);
-		dpurl_str = dpurl_str.concat("&AuthToken").concat(this.ivle.authToken);
-		dpurl_str = dpurl_str.concat("&CourseID=").concat(courseID);
-		dpurl_str = dpurl_str.concat("&UserID=").concat(this.userID);
-		URL dpurl = new URL(dpurl_str);
-		return dpurl;
+		StringBuilder builder = new StringBuilder()
+			.append("https://ivle.nus.edu.sg/api/DisplayPhoto.ashx?")
+			.append("APIKey=").append(this.ivle.apiKey)
+			.append("&AuthToken=").append(this.ivle.authToken)
+			.append("&CourseID=").append(courseID)
+			.append("&UserID=").append(this.userID);
+		String urlStr = builder.toString();
+		
+		// Make the request.
+		URL url = new URL(urlStr);
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		
+		// Read the response into a string first.
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		StringBuilder response = new StringBuilder();
+		String buf = null;
+		while ((buf = br.readLine()) != null) {
+			response.append(buf);
+		}
+		br.close();
+		
+		// Parse the response.
+		Document doc = Jsoup.parse(response.toString());
+		Element img = doc.select("img").first();
+		String imgUrlStr = img.attr("src");
+		URL imgUrl = new URL(imgUrlStr);
+		return imgUrl;
 	}
 	
 	/**
@@ -89,7 +125,8 @@ public class User extends IVLEObject {
 	 * 
 	 * @param courseID
 	 */
-	public InputStream getDisplayPhoto(String courseID) throws Exception {
+	public InputStream getDisplayPhoto(String courseID) throws
+			MalformedURLException, IOException {
 		// Get the URL.
 		URL dpurl = this.getDisplayPhotoURL(courseID);
 		
